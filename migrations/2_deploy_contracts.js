a25c573e22167eb58dd5c4b61a80a555670b0741
dravgust <dravgust@hotmail.com>
2021-12-05 20:09:03 +0200

    ...

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -16,9 +16,13 @@ module.exports = function(deployer) {
   //deployer.deploy(ApocalypseDesign);
   //deployer.deploy(Counter);
 
+  const minDelay = 0;
+  const proposers = ["0x1192aAa5F07ABD4F23939A1219a7165230D035d3"];
+  const executors = ["0x1192aAa5F07ABD4F23939A1219a7165230D035d3"];
+
   deployer.deploy(BlackToken)
     .then(() => BlackToken.deployed())
-    .then(() => deployer.deploy(TimelockController, 0, ["0x1192aAa5F07ABD4F23939A1219a7165230D035d3"], ["0x1192aAa5F07ABD4F23939A1219a7165230D035d3"]))
+    .then(() => deployer.deploy(TimelockController, minDelay, proposers, executors))
     .then(() => TimelockController.deployed())
     .then(() => deployer.deploy(BlackGovernor, BlackToken.address, TimelockController.address))
 
