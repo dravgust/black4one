3c5f4a2ee50289f131c3702e4cb19ed59e65ecfd
anton <anton@vayosoft.com>
2021-12-06 17:32:58 +0200

    ...

--- app/src/components/layouts/DefaultLayout.tsx
+++ app/src/components/layouts/DefaultLayout.tsx
@@ -1,58 +1,63 @@
 import React, { ReactNode } from "react";
-import { Flex, Box, Spacer, Heading, Grid, GridItem } from "@chakra-ui/react";
+import { Link, To } from "react-router-dom";
+import { Flex, Box, Spacer, Heading, Button, Grid, GridItem } from "@chakra-ui/react";
 import { useDisclosure } from "@chakra-ui/react";
 import AccountButton from "../account/AccountButton";
 import AccountModal from "../account/AccountModal";
 
 type Props = {
-  children?: ReactNode;
-  href?: string;
+    children?: ReactNode;
 };
 
-const MenuItems = ({ children, href }: Props) => (
-  <Box as="a" href={href} mt={{ base: 4, md: 0 }} mr={6} display="block">
-    {children}
-  </Box>
+type MenuItemsProps = {
+    children?: ReactNode;
+    href: To;
+};
+
+const MenuItems = ({ children, href }: MenuItemsProps) => (
+    <Link to={href}>
+        <Button bg="gray.700" color="white" borderRadius="xl" border="1px solid transparent" fontSize="md" m="1px" px={3}
+            _hover={{
+                borderColor: "blue.400"
+            }}>
+            {children}
+        </Button>
+    </Link>
 );
 
 const ChakraLayout = ({ children }: Props) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  return (
-    <Flex flexDirection="column" justifyContent="center" h="100vh" bg="gray.800">
-      <Flex p="4">
-        <Box>
-          <Heading size="md" color="gray.400">black4one</Heading>
-        </Box>
-        <Spacer />
-        <Box
-            display="flex"
-            width="auto"
-            flexGrow={1}
-            color="white"
-            size="md"
-          >
-            <MenuItems href="/">Balance</MenuItems>
-            <MenuItems href="/prices">Prices</MenuItems>
-            <MenuItems href="/block">Block</MenuItems>
-            <MenuItems href="/tokens">Tokens</MenuItems>
-            <MenuItems href="/send">Send Ether</MenuItems>
-            <MenuItems href="/transactions">Transactions</MenuItems>
-          </Box>
-        <Spacer />
-        <Box>
-          <AccountButton handleOpenModal={onOpen} />
-          <AccountModal isOpen={isOpen} onClose={onClose} />
-        </Box>
-      </Flex>
-      <Grid h="100%" templateRows="repeat(2, 1fr)" templateColumns="repeat(4, 1fr)" gap={6}>
-        <GridItem rowSpan={2} colSpan={1} />
-        <GridItem rowSpan={2} colSpan={2}>
-          {children}
-        </GridItem>
-        <GridItem rowSpan={2} colSpan={1} />
-      </Grid>
-    </Flex>
-  )
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    return (
+        <Flex flexDirection="column" bg="gray.800">
+            <Flex p="4">
+                <Box>
+                    <Heading size="md" color="gray.400"></Heading>
+                </Box>
+                <Spacer />
+                <Grid templateRows="repeat(2, 1fr)" gap={4}>
+                    <GridItem>
+                        {/* Desktop */}
+                        <Flex color="white" bg="gray.700" borderRadius="xl">
+                            <MenuItems href="/">Balance</MenuItems>
+                            <MenuItems href="/prices">Prices</MenuItems>
+                            <MenuItems href="/block">Block</MenuItems>
+                            <MenuItems href="/tokens">Tokens</MenuItems>
+                            <MenuItems href="/send">Send Ether</MenuItems>
+                            <MenuItems href="/transactions">Transactions</MenuItems>
+                        </Flex>
+                    </GridItem>
+                    <GridItem>
+                        {children}
+                    </GridItem>
+                </Grid>
+                <Spacer />
+                <Box>
+                    <AccountButton handleOpenModal={onOpen} />
+                    <AccountModal isOpen={isOpen} onClose={onClose} />
+                </Box>
+            </Flex>
+        </Flex>
+    )
 }
 
 export default ChakraLayout
