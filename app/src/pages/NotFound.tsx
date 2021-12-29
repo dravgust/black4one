44787168361d0812e5955d34b90e6ad17f2a63be
anton <anton@vayosoft.com>
2021-12-29 15:40:18 +0200

    ...

--- app/src/pages/NotFound.tsx
+++ app/src/pages/NotFound.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import LandingLayout from "../components/layouts/LandingLayout";
-//import Hero from "../components/sections/Hero";
+import Hero from "../components/sections/Hero";
 
 const NotFound = () => {
     return (
       <LandingLayout>
-      {/*<Hero
+      {<Hero
         title="Build this rad landing page from scratch"
         subtitle="This is the subheader section where you describe the basic benefits of your product"
         image="https://source.unsplash.com/collection/404339/800x600"
         ctaText="Create your account now"
         ctaLink="/signup"
-      />*/}
-      <div>test</div>
+      />}
     </LandingLayout>
     );
   }
