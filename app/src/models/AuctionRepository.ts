7ba91b7fcf4599ee8f52d7e59741a849faf51146
anton <anton@vayosoft.com>
2022-01-03 08:54:40 +0200

    ...

---
+++ app/src/models/AuctionRepository.ts
@@ -0,0 +1,31 @@
+import { BigNumber } from '@ethersproject/bignumber'
+
+export class Auction {
+    public name: string
+    public blockDeadline: BigNumber
+    public startPrice: BigNumber
+    public deedId: BigNumber
+    public metadata: string
+    public deedRepositoryAddress: string
+    public owner: string
+    public active: boolean
+    public finalized: boolean
+
+    constructor(auction: Auction){
+        this.name = auction.name
+        this.blockDeadline = auction.blockDeadline
+        this.startPrice = auction.startPrice
+        this.deedId = auction.deedId
+        this.metadata = auction.metadata
+        this.deedRepositoryAddress = auction.deedRepositoryAddress
+        this.owner = auction.owner
+        this.active = auction.active
+        this.finalized = auction.finalized
+
+    }
+}
+
+export class TokenAuction extends Auction {
+    public tokenId: number | null = null
+    public metadataURI: string = ''
+}
