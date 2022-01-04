77473c7e8733d31142ee87a1108905923fc031ba
anton <anton@vayosoft.com>
2022-01-04 17:17:48 +0200

    ...

--- app/src/utils/index.ts
+++ app/src/utils/index.ts
@@ -2,6 +2,6 @@ export * from './toHttpPath'
 export * from './ipfs'
 export * from './validation'
 
-export function range(size:number, startAt:number = 0):ReadonlyArray<number> {
-    return [...Array(size).keys()].map(i => i + startAt);
-  }
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const compose = (...fns: any) => (arg: any) => fns.reduce((composed: any, f: any) => f(composed), arg);
+export const range = (size:number, startAt:number = 0):ReadonlyArray<number> => [...Array(size).keys()].map(i => i + startAt)
